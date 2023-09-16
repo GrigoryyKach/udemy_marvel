8a5d7f37777c6b3835eb60563bbb6318e5622796
grygoryy <kachurovskyy123@gmail.com>
2023-09-16 20:26:32 +0300

    functions added

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,6 @@
 class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
-	_apiKey = 'apikey=e83bb7ced62fa6232e55e95bdbb6fbcf';
-
+	_apiKey = 'apikey=a5837db97d72016c81a7a776f4240db9'; //чужой ключ!
 	getResource = async (url) => {
 		let res = await fetch(url);
 
@@ -24,11 +23,13 @@ class MarvelService {
 
 	_transformCharacter = (char) => {
 		return {
+			id: char.id,
 			name: char.name,
 			description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
 			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
 			homepage: char.urls[0].url,
-			wiki: char.urls[1].url
+			wiki: char.urls[1].url,
+			comics: char.comics.items
 		}
 	}
 }
