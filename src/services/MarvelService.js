bcdd91cfa0239b05a37f53b5573a820ba0cd70dc
grygoryy <kachurovskyy123@gmail.com>
2023-09-19 20:59:00 +0300

    proptypes and load-more added

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
 	_apiKey = 'apikey=a5837db97d72016c81a7a776f4240db9'; //чужой ключ!
+	_baseOffset = 210;
 	getResource = async (url) => {
 		let res = await fetch(url);
 
@@ -11,8 +12,8 @@ class MarvelService {
 		return await res.json();
 	}
 
-	getAllCharacters = async () => {
-		const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+	getAllCharacters = async (offset = this._baseOffset) => {
+		const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
 		return res.data.results.map(this._transformCharacter);
 	}
 
