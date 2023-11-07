895cee47b67f0aada7a4c99ab33e7200f8b878a2
Grygoryy Kachurovskyy <kachurovskyy123@gmail.com>
2023-11-08 00:24:47 +0200

    dynamic routes added

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -53,7 +53,6 @@ const CharList = (props) => {
 			if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
 				imgStyle = { 'objectFit': 'unset' };
 			}
-
 			return (
 				<li
 					className="char__item"
